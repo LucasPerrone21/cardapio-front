51e073ddea291cc25caa506ed1bed4e7e643883a
Helen de Freitas <82972511+heleafreitas@users.noreply.github.com>
2025-07-15 18:19:10 -0300

    [feat] adiciona nova para enviar pedido ao carrinho

--- src/app/(root)/(features)/menu/adapters/menu.ts
+++ src/app/(root)/(features)/menu/adapters/menu.ts
@@ -5,3 +5,12 @@ export async function getMenu(): Promise<IProduct[]> {
     const data: IProductRequest[] = await res.json()
     return data.map((item) => ({ ...item, price: `R$ ${item.price.toFixed(2)}` }))
 }
+
+export async function postOrder(productId: number){
+    const res = await fetch('/api/order', { 
+        method: 'POST', 
+        body: JSON.stringify({id: productId, count: 1})
+     })
+
+    return res.status
+}
